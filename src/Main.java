b3f575796c6053eb0f9ba1227574e51291bf80cf
Dmitriy <dkentry@yandex.ru>
2023-06-05 23:13:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        RedBlackTree tree = new RedBlackTree();
+
+        tree.add(3);
+        tree.add(6);
+        tree.add(9);
+
+        System.out.println(tree);
+    }
+}
